2926f3c06d3d6748a417b838686b8def2fb1350b
jayrock3000 <jeffrhoten@gmail.com>
2024-11-25 12:47:29 -0800

    Added notes for next time

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from pyswip import Prolog
 
 prolog = Prolog()
 
+
+# Build a for importing prolog statements here as a document, then parse it into these commands
+# simple line break for delineation of commands
+# each one becomes an assertz in a loop
+
 prolog.assertz("father(michael, john)")
 prolog.assertz("father(michael, craig)")
 
@@ -13,5 +18,6 @@ for result in prolog.query("father(X, Y)"):
 print(result)
 '''
 
+# Section for querying prolog
 for result in prolog.query("father(michael, X)"):
     print(result)
