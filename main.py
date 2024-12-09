dd9543db954a96fcbb9c6947b442a86075a4c42d
jayrock3000 <jeffrhoten@gmail.com>
2024-12-09 10:43:39 -0800

    Implemented rawComposition

--- main.py
+++ main.py
@@ -25,14 +25,26 @@ for result in prolog.query("father(michael, X)"):
 """
 #prolog.assertz("")
 prolog.assertz("raw(redstone)")
-prolog.assertz("comparator(redstone, 3)")
-prolog.assertz("comparator(stick, 2)")
-prolog.assertz("nonRaw(X) :- not(raw(X))")
+prolog.assertz("raw(log)")
+prolog.assertz("composedOf(redstoneTorch, redstone, 12)")
+prolog.assertz("composedOf(redstoneTorch, stick, 1)")
+prolog.assertz("composedOf(comparator, redstoneTorch, 3)")
+prolog.assertz("rawComposition(Item, Component, Qty) :- composedOf(Item, Component, Qty), raw(Component)")
+prolog.assertz("rawComposition(Item, Subcomponent, TotalQty) :- composedOf(Item, Component, Qty), rawComposition(Component, Subcomponent, SubQty), TotalQty is Qty * SubQty")
+#prolog.assertz("")
+#prolog.assertz("")
+#prolog.assertz("")
+#prolog.assertz("")
+#prolog.assertz("")
+#prolog.assertz("")
+#prolog.assertz("")
 #prolog.assertz("")
 
-
-query = "thingy(X, Y)"
-result = list(prolog.query(query))
+query = "rawComposition(comparator, X, Y)"
+#result = list(prolog.query(query))
+#print(result)
+for result in prolog.query(query):
+    print(result)
 
 # X now the object you're creating
 # Y is result[0][X] now a list of the components
@@ -44,7 +56,8 @@ def main():
     print("Program begins")
     finalResources = {}         # Dictionary to tabulate all resources needed
     finalProcedure = []         # List to document steps for player to follow
-    farmCutoff = {}             # Amount of resources where a farm will be recommended (instead of gathering manually)
+    #farmCutoff = {}             # Amount of resources where a farm will be recommended (instead of gathering manually)
+    #^ do this in prolog
 
     finalResources.update({"redstone": 3, "stone": 16})
     finalProcedure.insert(0, "Get money")
