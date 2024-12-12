49f0eca7377e4aaf055777ea2ce38e061437dda5
jayrock3000 <jeffrhoten@gmail.com>
2024-12-12 14:07:44 -0800

    Added facts to KB

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pyswip import Prolog
 # Global Variables
 PROLOG = Prolog()
 DEBUG = False
-VALID_MACHINES = ["stone", "plank", "stick", "redstonetorch", "comparator", "witchfarm"]
+VALID_MACHINES = ["comparator", "observer", "repeater", "witchfarm", "itemsorter", "piston", "stickypiston", "raidfarm", "villagerfarm"]
 QUIT_WORDS = ["", "3", "exit", "close", "quit"]
 MAX_STRING_LENGTH = 99
 EFFORT_CONVERSION = {"verylow" : 1, "low" : 3, "medium" : 5, "high" : 7, "veryhigh" : 9}
@@ -92,25 +92,69 @@ def prologInitialize():
     PROLOG.assertz("raw(string)")
     PROLOG.assertz("raw(emerald)")
     PROLOG.assertz("raw(villager)")
+    PROLOG.assertz("raw(slime)")
+    PROLOG.assertz("raw(iron)")
+    PROLOG.assertz("raw(honey)")
+    PROLOG.assertz("raw(obsidian)")
+    PROLOG.assertz("raw(terracotta)")
+    PROLOG.assertz("raw(sand)")
+    PROLOG.assertz("raw(spiderspawner)")
+    PROLOG.assertz("raw(zombiespawner)")
+    PROLOG.assertz("raw(skeletonspawner)")
+    PROLOG.assertz("raw(copper)")
+    PROLOG.assertz("raw(glowstone)")
+    PROLOG.assertz("raw(ice)")
+    PROLOG.assertz("raw(bamboo)")
+    PROLOG.assertz("raw(goldnugget)")
+    PROLOG.assertz("raw(shulkershell)")
+    PROLOG.assertz("raw(turtleegg)")
+    PROLOG.assertz("raw(coral)")
+    PROLOG.assertz("raw(magmablock)")
+    PROLOG.assertz("raw(soulsand)")
+    PROLOG.assertz("raw(gunpowder)")
+    PROLOG.assertz("raw(spidereye)")
+    PROLOG.assertz("raw(bottle)")
+    #PROLOG.assertz("raw()")
+    #PROLOG.assertz("raw()")
+
 
     PROLOG.assertz("notRaw(Item) :- not(raw(Item))")
 
     
     # Farm cutoffs for raw resources
-    '''
-    PROLOG.assertz("farmCutoff(redstone, 200)")
+    PROLOG.assertz("farmCutoff(redstone, 64)")
     PROLOG.assertz("farmCutoff(log, 400)")
     PROLOG.assertz("farmCutoff(quartz, 200)")
-    PROLOG.assertz("farmCutoff(cobblestone, 1000)")
+    PROLOG.assertz("farmCutoff(cobblestone, 2000)")
     PROLOG.assertz("farmCutoff(stick, 200)")
     PROLOG.assertz("farmCutoff(string, 64)")
-    '''
-    PROLOG.assertz("farmCutoff(redstone, 1)")
-    PROLOG.assertz("farmCutoff(log, 1)")
-    PROLOG.assertz("farmCutoff(quartz, 100)")
-    PROLOG.assertz("farmCutoff(cobblestone, 1)")
-    PROLOG.assertz("farmCutoff(stick, 1)")
-    PROLOG.assertz("farmCutoff(string, 1)")
+    PROLOG.assertz("farmCutoff(emerald, 200)")
+    PROLOG.assertz("farmCutoff(villager, 2)")
+    PROLOG.assertz("farmCutoff(slime, 10)")
+    PROLOG.assertz("farmCutoff(iron, 64)")
+    PROLOG.assertz("farmCutoff(honey, 32)")
+    PROLOG.assertz("farmCutoff(obsidian, 128)")
+    PROLOG.assertz("farmCutoff(terracotta, 2000)")
+    PROLOG.assertz("farmCutoff(sand, 2000)")
+    PROLOG.assertz("farmCutoff(spiderspawner, 1)")
+    PROLOG.assertz("farmCutoff(zombiespawner, 1)")
+    PROLOG.assertz("farmCutoff(skeletonspawner, 1)")
+    PROLOG.assertz("farmCutoff(copper, 1000)")
+    PROLOG.assertz("farmCutoff(glowstone, 200)")
+    PROLOG.assertz("farmCutoff(ice, 2000)")
+    PROLOG.assertz("farmCutoff(bamboo, 400)")
+    PROLOG.assertz("farmCutoff(goldnugget, 500)")
+    PROLOG.assertz("farmCutoff(shulkershell, 32)")
+    PROLOG.assertz("farmCutoff(turtleegg, 1)")
+    PROLOG.assertz("farmCutoff(coral, 64)")
+    PROLOG.assertz("farmCutoff(magmablock, 500)")
+    PROLOG.assertz("farmCutoff(soulsand, 1000)")
+    PROLOG.assertz("farmCutoff(gunpowder, 64)")
+    PROLOG.assertz("farmCutoff(spidereye, 32)")
+    PROLOG.assertz("farmCutoff(bottle, 1000)")
+    #PROLOG.assertz("farmCutoff(, )")
+    #PROLOG.assertz("farmCutoff(, )")
+
 
     # item composition
     PROLOG.assertz("composedOf(stone, cobblestone, 1)")
@@ -121,52 +165,124 @@ def prologInitialize():
     PROLOG.assertz("composedOf(comparator, stone, 3)")
     PROLOG.assertz("composedOf(stick, plank, 0.5)")
     PROLOG.assertz("composedOf(plank, log, 0.25)")
+    PROLOG.assertz("composedOf(observer, cobblestone, 6)")
+    PROLOG.assertz("composedOf(observer, redstone, 2)")
+    PROLOG.assertz("composedOf(observer, quartz, 1)")
+    PROLOG.assertz("composedOf(repeater, redstone, 1)")
+    PROLOG.assertz("composedOf(repeater, redstonetorch, 2)")
+    PROLOG.assertz("composedOf(repeater, stone, 3)")
+    PROLOG.assertz("composedOf(piston, plank, 3)")
+    PROLOG.assertz("composedOf(piston, cobblestone, 4)")
+    PROLOG.assertz("composedOf(piston, redstone, 1)")
+    PROLOG.assertz("composedOf(piston, iron, 1)")
+    PROLOG.assertz("composedOf(stickypiston, piston, 1)")
+    PROLOG.assertz("composedOf(stickypiston, slime, 1)")
+    PROLOG.assertz("composedOf(slimeblock, slime, 9)")
+    PROLOG.assertz("composedOf(redstoneblock, redstone, 9)")
+    PROLOG.assertz("composedOf(honeyblock, honey, 4)")
+    PROLOG.assertz("composedOf(glazedterracotta, terracotta, 1)")
+    PROLOG.assertz("composedOf(glass, sand, 1)")
+    PROLOG.assertz("composedOf(chest, plank, 8)")
+    PROLOG.assertz("composedOf(hopper, chest, 1)")
+    PROLOG.assertz("composedOf(hopper, iron, 5)")
+    PROLOG.assertz("composedOf(dropper, cobblestone, 7)")
+    PROLOG.assertz("composedOf(dropper, redstone, 1)")
+    PROLOG.assertz("composedOf(dispenser, cobblestone, 7)")
+    PROLOG.assertz("composedOf(dispenser, redstone, 1)")
+    PROLOG.assertz("composedOf(dispenser, bow, 1)")
+    PROLOG.assertz("composedOf(bow, stick, 3)")
+    PROLOG.assertz("composedOf(bow, string, 3)")
+    PROLOG.assertz("composedOf(redstonelamp, redstone, 4)")
+    PROLOG.assertz("composedOf(redstonelamp, glowstoneblock, 1)")
+    PROLOG.assertz("composedOf(glowstoneblock, glowstone, 4)")
+    PROLOG.assertz("composedOf(lever, cobblestone, 1)")
+    PROLOG.assertz("composedOf(lever, stick, 1)")
+    PROLOG.assertz("composedOf(stonebutton, stone, 1)")
+    PROLOG.assertz("composedOf(woodbutton, plank, 1)")
+    PROLOG.assertz("composedOf(packedice, ice, 9)")
+    PROLOG.assertz("composedOf(scaffolding, bamboo, 1)")
+    PROLOG.assertz("composedOf(scaffolding, string, 0.2)")
+    PROLOG.assertz("composedOf(goldingot, goldnugget, 9)")
+    PROLOG.assertz("composedOf(shulkerbox, shulkershell, 2)")
+    PROLOG.assertz("composedOf(shulkerbox, chest, 1)")
+    PROLOG.assertz("composedOf(itemsorter, comparator, 1)")
+    PROLOG.assertz("composedOf(itemsorter, redstone, 3)")
+    PROLOG.assertz("composedOf(itemsorter, repeater, 1)")
+    PROLOG.assertz("composedOf(itemsorter, hopper, 3)")
+    PROLOG.assertz("composedOf(itemsorter, chest, 1)")
+    PROLOG.assertz("composedOf(trapdoor, plank, 3)")
+    PROLOG.assertz("composedOf(deadcoral, coral, 1)")
+    PROLOG.assertz("composedOf(plankslab, plank, 0.5)")
+    PROLOG.assertz("composedOf(composter, plankslab, 7)")
+    PROLOG.assertz("composedOf(bucket, iron, 3)")
+    PROLOG.assertz("composedOf(lava, bucket, 1)")
+    PROLOG.assertz("composedOf(tnt, gunpowder, 5)")
+    PROLOG.assertz("composedOf(tnt, sand, 4)")
+    PROLOG.assertz("composedOf(tripwirehook, iron, 1)")
+    PROLOG.assertz("composedOf(tripwirehook, plank, 1)")
+    PROLOG.assertz("composedOf(tripwirehook, stick, 1)")
+    PROLOG.assertz("composedOf(minecart, iron, 5)")
+    #PROLOG.assertz("composedOf(, , )")
+    #PROLOG.assertz("composedOf(, , )")
+    #PROLOG.assertz("composedOf(, , )")
+    #PROLOG.assertz("composedOf(, , )")
+    #PROLOG.assertz("composedOf(, , )")
+    #PROLOG.assertz("composedOf(, , )")
+
+
+    # Get the raw materials composing a component
     PROLOG.assertz("rawComposition(Item, Component, Qty) :- composedOf(Item, Component, Qty), raw(Component)")  # Returns raw components of an item
     PROLOG.assertz("rawComposition(Item, Subcomponent, TotalQty) :- composedOf(Item, Component, Qty), notRaw(Item), rawComposition(Component, Subcomponent, SubQty), TotalQty is Qty * SubQty") # Recursively calls subcomponents until a raw item is found
 
-    # mats
-    PROLOG.assertz("composedOf(plankslab, plank, 0.5)")
-    PROLOG.assertz("composedOf(composter, plankslab, 7)")
-
-    # Materials to create
-    PROLOG.assertz("composedOf(witchfarm, composter, 10)")
-    PROLOG.assertz("composedOf(witchfarm, string, 50)")
-    PROLOG.assertz("composedOf(villagerfarm, villager, 2)")
-
-    # Drops from farm
-    PROLOG.assertz("produces(witchfarm, redstone, 250)")
+    # FARMS - track what they produce, materials required to make them, and how much effort it takes
     PROLOG.assertz("produces(witchfarm, stick, 500)")
-    PROLOG.assertz("produces(villagerfarm, redstone, 100)")
-    PROLOG.assertz("produces(raidfarm, redstone, 1000)")
-    PROLOG.assertz("produces(raidfarm, stick, 2000)")
-    PROLOG.assertz("produces(raidfarm, emerald, 5000)")
+    PROLOG.assertz("produces(witchfarm, redstone, 250)")
+    PROLOG.assertz("produces(witchfarm, glowstone, 250)")
+    PROLOG.assertz("produces(witchfarm, gunpowder, 250)")
+    PROLOG.assertz("produces(witchfarm, spidereye, 250)")
+    PROLOG.assertz("produces(witchfarm, bottle, 250)")
+    PROLOG.assertz("produces(witchfarm, sugar, 250)")
+    PROLOG.assertz("composedOf(witchfarm, composter, 21)")
+    PROLOG.assertz("composedOf(witchfarm, string, 49)")
+    PROLOG.assertz("composedOf(witchfarm, stickypiston, 21)")
+    PROLOG.assertz("composedOf(witchfarm, comparator, 21)")
+    PROLOG.assertz("composedOf(witchfarm, trapdoor, 21)")
+    PROLOG.assertz("composedOf(witchfarm, tripwirehook, 42)")
+    PROLOG.assertz("composedOf(witchfarm, minecart, 24)")
+    PROLOG.assertz("composedOf(witchfarm, itemsorter, 7)")
+    PROLOG.assertz("effort(witchfarm, low)")
 
-    # Effort to build farm
-    PROLOG.assertz("effort(witchfarm, verylow)")
+    PROLOG.assertz("produces(villagerfarm, redstone, 50)")
+    PROLOG.assertz("produces(villagerfarm, emerald, 100)")
+    PROLOG.assertz("produces(villagerfarm, villager, 100)")
+    PROLOG.assertz("composedOf(villagerfarm, villager, 2)")
     PROLOG.assertz("effort(villagerfarm, medium)")
+
+    PROLOG.assertz("produces(raidfarm, stick, 1000)")
+    PROLOG.assertz("produces(raidfarm, redstone, 500)")
+    PROLOG.assertz("produces(raidfarm, glowstone, 500)")
+    PROLOG.assertz("produces(raidfarm, gunpowder, 500)")
+    PROLOG.assertz("produces(raidfarm, spidereye, 500)")
+    PROLOG.assertz("produces(raidfarm, bottle, 500)")
+    PROLOG.assertz("produces(raidfarm, sugar, 500)")
+    PROLOG.assertz("produces(raidfarm, emerald, 2000)")
+    PROLOG.assertz("composedOf(raidfarm, villager, 4)")
+    PROLOG.assertz("composedOf(raidfarm, composter, 4)")
+    PROLOG.assertz("composedOf(raidfarm, glass, 40)")
+    PROLOG.assertz("composedOf(raidfarm, magmablock, 16)")
+    PROLOG.assertz("composedOf(raidfarm, lava, 4)")
+    PROLOG.assertz("composedOf(raidfarm, soulsand, 16)")
+    PROLOG.assertz("composedOf(raidfarm, piston, 16)")
+    PROLOG.assertz("composedOf(raidfarm, comparator, 4)")
+    PROLOG.assertz("composedOf(raidfarm, repeater, 4)")
+    PROLOG.assertz("composedOf(raidfarm, dispenser, 1)")
+    PROLOG.assertz("composedOf(raidfarm, dropper, 2)")
+    PROLOG.assertz("composedOf(raidfarm, itemsorter, 14)")
     PROLOG.assertz("effort(raidfarm, veryhigh)")
+
+
     #PROLOG.assertz("")
     #PROLOG.assertz("")
-
-    #Witch Farm
-    #
-    # Resources it produces
-    #   PRODUCES:   Redstone    250     per hour
-    #   PRODUCES:   Sticks      250     per hour
-    #   PRODUCES:   etc         ...     ...
-    #
-    # Resources to build it:
-    #   RESOURCES:  Composters  10
-    #   RESOURCES:  String      50
-    #   RESOURCES:  etc         ...
-    #           
-    # Effort to build it:
-    #   EFFORT:     low
-
-############TO DO##########
-############TO DO##########
-############TO DO##########
-
 
 
 
@@ -209,19 +325,8 @@ def getUserQuery():
     resourcesNeeded = {}        # Working list of all resources our algorithm must account for
     resourcesToGather = {}      # Resources the player should gather manually
     resourcesFinal = {}         # Dictionary to tabulate all resources needed
-    resourcesFound = []
+    resourcesFound = []         # Tracks resources produced by farms recommended (so far)
     procedureFinal = []         # List to document steps for player to follow
-    '''
-    #farmCutoff = {}             # Amount of resources where a farm will be recommended (instead of gathering manually)
-    #^ do this in prolog
-
-    # Initialize any preexisting resources
-    #resourcesFinal["redstone"] = 15
-
-    # Initialize any preexisting procedures
-    #procedureFinal.insert(0, "Get money")
-    #procedureFinal.insert(0, "Fuck bitches")
-    '''
 
     # Get User Query
     userQueryInput = ""
@@ -252,7 +357,7 @@ def getUserQuery():
     # Add building the machine to the procedure
     procedureFinal.append(f"Build {userQueryInput}")
 
-    # Build Procedure               # need a resourcesNeeded variable to track in general, a list which gets reduced to zero over time, loop until it's empty
+    # Build Procedure
     while True:
         addedResources = {}
 
@@ -288,8 +393,6 @@ def getUserQuery():
             farmQuery = f"produces(Farmname, {resource}, Amt)"
             farm = {}       # Variable to receive the result of the prolog query
             for farm in PROLOG.query(farmQuery):
-                #print("Result of the query was:")
-                #print(farm)
                 farmsList.update({farm["Farmname"]:farm["Amt"]})
 
             # Handle exceptions where no farms can be found
@@ -335,7 +438,6 @@ def getUserQuery():
 
                 # Set suitability score for this farm
                 suitabilityScore.update({thisFarm : score})
-                #print(f"Suitability score for {thisFarm} is {suitabilityScore[thisFarm]}")
 
             # Get farm with max score
             bestFarm = ""
@@ -372,7 +474,6 @@ def getUserQuery():
             resourcesNeeded = addedResources
             for x in addedResources:
                 resourcesFinal.update({x : addedResources[x]})
-            #print(f"resourcesNeeded is now: {resourcesNeeded}, searching again")
             print(f"\nDetermined additional resources needed to consturct farms, searching again")
         #break
 
